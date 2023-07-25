4f8f8c63ea2443368726275ee40791dc481cc69d
Mahdy Asady <mahdy.asady@gmail.com>
2023-07-25 23:06:28 +0330

    decoder app initialized

---
+++ decoder.py
@@ -0,0 +1,14 @@
+import csv
+
+def readRecordsFromDB(fileName):
+    records = {}
+    with open(fileName, 'r') as csvFile:
+        dataReader = csv.reader(csvFile)
+        for row in dataReader:
+            id = row.pop(0)
+            records[id] = row
+    return records
+
+##########################################################################
+db = readRecordsFromDB('sample.db')
+print(db)
