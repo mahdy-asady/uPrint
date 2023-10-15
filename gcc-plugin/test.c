7d15c386783275ef798a409b5ce1ec772205fc47
Mahdy Asady <mahdy.asady@gmail.com>
2023-10-15 20:27:52 +0330

    Test recipe added to makefile

---
+++ gcc-plugin/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+void output()
+{
+    printf("Do something here!\n");
+}
+
+int f1() {
+    return 1;
+}
+
+void f2()
+{
+    if(f1() == 1)
+        sampleFn("Sample function f2() true condition call\n");
+    else 
+        sampleFn("Sample function f2() false condition call\n");
+    return;
+}
+
+void f3()
+{
+    int a;
+    a = a + 1;
+    printf("a = %d!\n", a);
+    if(a > 1)
+        f3();
+    else 
+        f2();
+    return;
+}
+
+int main()
+{
+    printf("I'm a target C program.\n");
+    f3();
+    f2();
+    return 1;
+}
